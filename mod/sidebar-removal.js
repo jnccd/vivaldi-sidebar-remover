e8d5725da1052ba17488044f14a6b065d821f526
niklasCarstensen <niklas.carstensen@gmx.de>
2020-12-14 23:49:47 +0100

    Readd symbols after fullscreen

--- mod/sidebar-removal.js
+++ mod/sidebar-removal.js
@@ -8,43 +8,51 @@ window.onpageshow = applyMod;
 window.onmousemove = applyMod;
 
 var applied = false
+var clonedChildren = []
 
 function applyMod() {
-    if (applied)
-        return;
-
-    // Skip if browser didn't load yet
-    if (!document.body.contains(document.getElementById('switch')) || 
-        !document.body.contains(document.getElementsByClassName('button-toolbar button-circularimage')[0]))
+    
+    var ubar = document.getElementsByClassName('button-toolbar button-circularimage')[0]
+    if (!document.body.contains(ubar) || ubar.children.length !== 1)
         return;
     
     // --- Core script ---------------------------------------------------------------------
     
-    var ubar = document.getElementsByClassName('button-toolbar button-circularimage')[0];
-    var sw = document.getElementById('switch');
-
-    while (sw.childElementCount > 2) {
-        var curChild = sw.children[0]
-        sw.removeChild(curChild)
-        ubar.appendChild(curChild)
+    var sw = document.getElementById('switch')
+    
+    if (sw !== null) {
+        while (sw.childElementCount > 2) {
+            var curChild = sw.children[0]
+            
+            clonedChildren.push(curChild.cloneNode(true))
+            //clonedChildren[clonedChildren.length - 1].
+            
+            sw.removeChild(curChild)
+            ubar.appendChild(curChild)
+        }
+    } else {
+        clonedChildren.forEach(x => ubar.appendChild(x))
     }
 
     var pan = document.getElementById('panels')
     var cont = document.getElementById('panels-container')
     var gr = document.getElementsByClassName('panel-group')[0]
 
-    pan.removeChild(sw)
+    if (sw !== null) pan.removeChild(sw)
     cont.style.width = 0
-    gr.style.right = 0 
-    
-    cont.ontransitionend = function() { 
+    if (gr !== undefined) gr.style.right = 0
+        
+    cont.ontransitionend = function() {
+        gr = document.getElementsByClassName('panel-group')[0]
         cont.style.width = 0
-        gr.style.right = 0 
-        var left = parseInt(cont.children[1].style.left,10);
-        cont.children[1].style.left = left + 34
+        if (gr !== undefined) gr.style.right = 0
+        if (cont.children[1] !== undefined) {
+            var left = parseInt(cont.children[1].style.left,10)
+            cont.children[1].style.left = left + 34
+        }
     }
 
     // --- Core script ---------------------------------------------------------------------
     
-    applied = true;
+    applied = true
 }
